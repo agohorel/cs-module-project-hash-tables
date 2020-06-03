c0f2671395a06b43a944ff04b31478aaeccd7beb
agohorel <3317203+agohorel@users.noreply.github.com>
2020-06-03 16:13:03 -0400

    add word count solution

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,10 +1,24 @@
 def word_count(s):
-    # Your code here
+    counts = {}
+    ignore = '"":;,.-+=/\\|[]{}()*^&'
+    clean_str = s.lower().replace("\r", " ").replace(
+        "\t", " ").replace("\n", " ").split(" ")
 
+    for word in clean_str:
+        word = word.strip(ignore)
+        if word not in counts and word != "":
+            counts[word] = 1
+        elif word != "":
+            counts[word] += 1
+
+    return counts
 
 
 if __name__ == "__main__":
     print(word_count(""))
     print(word_count("Hello"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
-    print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+    print(word_count(
+        'This is a test of the emergency broadcast network. This is only a test.'))
+    print(word_count('":;,.-+=/\\|[]{}()*^&'))
+    print(word_count('a a\ra\na\ta \t\r\n'))
